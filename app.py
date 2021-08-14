cd0f5778a34a78fdc0d9e84c6e97109e4245ea41
abhiroopdutta <abhiroopd@ymail.com>
2021-08-14 15:20:19 +0530

    adding flask for backend

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template,jsonify
+from flaskext.mysql import MySQL
+mysql = MySQL()
+
+app = Flask(__name__)
+mysql.init_app(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Eureka1263'
+app.config['MYSQL_DATABASE_DB'] = 'tyres'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+conn = mysql.connect()
+cursor =conn.cursor()
+cursor.execute("SELECT * from pv_tyres WHERE QUANTITY>0")
+data = list(cursor.fetchall())
+
+@app.route("/")
+def hello_world():
+    return jsonify(data)
