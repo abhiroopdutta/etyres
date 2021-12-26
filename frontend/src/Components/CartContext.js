a6acb2958888561d75e7773c0c6e7420d3796482
abhiroopdutta <abhiroopd@ymail.com>
2021-12-26 19:02:45 +0530

    fixed trailing whitespace in services HSN

--- frontend/src/Components/CartContext.js
+++ frontend/src/Components/CartContext.js
@@ -7,9 +7,9 @@ export const CartProvider = (props) => {
     const [cart, setCart] = useState([]);
     const [services, setServices] = useState([
         {name:"Fitting", price:0, quantity:0, HSN:"998714"}, 
-        {name:"Balancing", price:0, quantity:0, HSN:"998714 "},
+        {name:"Balancing", price:0, quantity:0, HSN:"998714"},
         {name:"Weights", price:0, quantity:0, HSN:"-"},
-        {name:"Alignment", price:0, quantity:0, HSN:"998714 "}])
+        {name:"Alignment", price:0, quantity:0, HSN:"998714"}])
     return(
         <CartContext.Provider value={ {tyresContext:[cart, setCart], servicesContext:[services, setServices]} }>
             {props.children}
