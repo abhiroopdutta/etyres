09e32708cd0940f58535491d02c8b6cc5098bfdc
abhiroopdutta <abhiroopd@ymail.com>
2022-03-20 12:17:02 +0530

    improved css for modal

--- frontend/src/Components/AddItem.js
+++ frontend/src/Components/AddItem.js
@@ -13,28 +13,28 @@ function AddItem({item, toggleModal}){
     return (
         <div className="add-item-modal">
             <div className="add-item-modal-content">
-                <header className="add-item-child">
+                <header className="add-item-header">
                     <strong>Add Item to inventory</strong>
                     <button onClick={handleCloseModal}>X</button>
                 </header>    
 
-                <section className="add-item-child">
-                    <h4>{item.item_desc}</h4>
-                    <h4>{item.item_code}</h4>
+                <section className="add-item-body">
+                    <strong>{item.item_desc}</strong>
+                    <strong>{item.item_code}</strong>
                     <label htmlFor="cost-price">Cost Price:</label>
                     <input type="text" id="cost-price" name="cost-price" value={roundToTwo(item.item_total/item.quantity)}/>
-                      <label for="vehicle-category">Category</label>
-                        <select name="vehicle-category">
-                          <option value="passenger car">PCR</option>
-                          <option value="2 wheeler">2 Wheeler</option>
-                          <option value="3 wheeler">3 Wheeler</option>
-                          <option value="scv">SCV</option>
-                          <option value="tubeless valve">Tubeless Valve</option>
-                        </select>
+                    <label for="vehicle-category">Category</label>
+                      <select name="vehicle-category">
+                        <option value="passenger car">PCR</option>
+                        <option value="2 wheeler">2 Wheeler</option>
+                        <option value="3 wheeler">3 Wheeler</option>
+                        <option value="scv">SCV</option>
+                        <option value="tubeless valve">Tubeless Valve</option>
+                      </select>
                     
                 </section>
 
-                <footer className="add-item-child">
+                <footer className="add-item-footer">
                     <button>Add to inventory</button>
                 </footer>
             </div>
