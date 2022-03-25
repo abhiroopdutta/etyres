6d53191651d3cb901ca517db23512fc312959818
abhiroopdutta <abhiroopd@ymail.com>
2022-03-25 21:42:53 +0530

    updated dir struct

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import CreateOrder from './Components/CreateOrder';
-import UpdatePrice from './Components/UpdatePrice';
-import UpdateStock from './Components/UpdateStock';
-import SalesReport from './Components/SalesReport';
+import CreateOrder from './Components/CreateOrder/CreateOrder';
+import UpdatePrice from './Components/UpdatePrice/UpdatePrice';
+import UpdateStock from './Components/UpdateStock/UpdateStock';
+import SalesReport from './Components/SalesReport/SalesReport';
 
 function App() {
   return (
