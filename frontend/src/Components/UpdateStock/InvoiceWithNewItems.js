9087850b6ea241bf085bea90664997b5091274db
abhiroopdutta <abhiroopd@ymail.com>
2022-04-22 00:46:59 +0530

    added thead, tbody, etc, added prettier formatter

--- frontend/src/Components/UpdateStock/InvoiceWithNewItems.js
+++ frontend/src/Components/UpdateStock/InvoiceWithNewItems.js
@@ -1,77 +1,94 @@
-import { useEffect, useState } from 'react';
-import './InvoiceWithNewItems.css';
-import AddItem from './AddItem';
-import React from 'react';
+import { useEffect, useState } from "react";
+import "./InvoiceWithNewItems.css";
+import AddItem from "./AddItem";
+import React from "react";
 
+function InvoiceWithNewItems({
+  initial_invoice,
+  invoiceIndex,
+  convertToNormalInvoice,
+}) {
+  const [showModal, setShowModal] = useState(false);
+  const [item, setItem] = useState();
+  const [invoice, setInvoice] = useState(initial_invoice);
 
-function InvoiceWithNewItems ({initial_invoice, invoiceIndex, convertToNormalInvoice}) {
-
-    const [showModal, setShowModal] = useState(false);
-    const [item, setItem] = useState();
-    const [invoice, setInvoice] = useState(initial_invoice);
-
-    useEffect(() => {
-        let notFoundInInventory = invoice.items.find(item => item.not_found_in_inventory === true);
-
-        if (!notFoundInInventory){
-            convertToNormalInvoice(invoice);
-        }
-    }, [invoice]);
-
-    const toggleModal = (state) => {
-        setShowModal(state);
-    };
-
-    const handleUpdateInventory = item => {
-        setItem(item);
-        toggleModal(true);
-    };
-
-    const updateItemStatus = (new_item) => {
-        let invoiceCopy = {...invoice};
-        let itemIndex = invoiceCopy.items.findIndex(item => 
-            item.item_code === new_item.item_code);
-        invoiceCopy.items[itemIndex].not_found_in_inventory = false;
-        setInvoice(invoiceCopy);
-    }
-
-    return (
-        <div className="invoice-with-new-items">
-            <header>
-                <strong className="invoice-number">Invoice no. {invoice.invoice_number}</strong> 
-            </header>
-            <hr/>
-            <table className="invoice-item-headers">
-                <tr>
-                    <th>S.no.</th>
-                    <th>Item Desc</th>
-                    <th>Qty</th>
-                    <th>Price</th>
-                    <th>inventory</th>
-                </tr>
-                
-                {invoice.items.map( (item, index)=>
-                <tr key={index}>
-                    <td>{index+1}</td>
-                    <td>{item.item_desc}:</td> 
-                    <td>{item.quantity}</td>
-                    <td>{item.item_total}</td>
-                    <td>{item.not_found_in_inventory?
-                        <button onClick = {()=>handleUpdateInventory(item)}>Add to inventory</button>
-                        : <div>&#9989;</div>}
-                    </td>
-                </tr>)}
-            </table>
-
-            {showModal?
-            <AddItem
-                item = {item}
-                toggleModal = {toggleModal}
-                updateItemStatus = {updateItemStatus}
-            />
-            :null}
-        </div>
+  useEffect(() => {
+    let notFoundInInventory = invoice.items.find(
+      (item) => item.not_found_in_inventory === true
     );
+
+    if (!notFoundInInventory) {
+      convertToNormalInvoice(invoice);
+    }
+  }, [invoice]);
+
+  const toggleModal = (state) => {
+    setShowModal(state);
+  };
+
+  const handleUpdateInventory = (item) => {
+    setItem(item);
+    toggleModal(true);
+  };
+
+  const updateItemStatus = (new_item) => {
+    let invoiceCopy = { ...invoice };
+    let itemIndex = invoiceCopy.items.findIndex(
+      (item) => item.item_code === new_item.item_code
+    );
+    invoiceCopy.items[itemIndex].not_found_in_inventory = false;
+    setInvoice(invoiceCopy);
+  };
+
+  return (
+    <div className="invoice-with-new-items">
+      <header>
+        <strong className="invoice-number">
+          Invoice no. {invoice.invoice_number}
+        </strong>
+      </header>
+      <hr />
+      <table className="invoice-item-headers">
+        <thead>
+          <tr>
+            <th>S.no.</th>
+            <th>Item Desc</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>inventory</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {invoice.items.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.item_desc}:</td>
+              <td>{item.quantity}</td>
+              <td>{item.item_total}</td>
+              <td>
+                {item.not_found_in_inventory ? (
+                  <button onClick={() => handleUpdateInventory(item)}>
+                    Add to inventory
+                  </button>
+                ) : (
+                  <div>&#9989;</div>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {showModal ? (
+        <AddItem
+          item={item}
+          toggleModal={toggleModal}
+          updateItemStatus={updateItemStatus}
+        />
+      ) : null}
+    </div>
+  );
 }
 
 export default InvoiceWithNewItems;
--- frontend/src/Components/UpdateStock/PurchaseInvoice.js
+++ frontend/src/Components/UpdateStock/PurchaseInvoice.js
@@ -1,111 +1,178 @@
-import React, { useEffect, useState } from 'react';
-import './PurchaseInvoice.css';
+import React, { useEffect, useState } from "react";
+import "./PurchaseInvoice.css";
 
-function PurchaseInvoice({invoice, invoice_index, handleInvoiceDate, handleClaimOverwrite, handleOverwrite, handleClaimNumber, handleSpecialDiscount}){
+function PurchaseInvoice({
+  invoice,
+  invoice_index,
+  handleInvoiceDate,
+  handleClaimOverwrite,
+  handleOverwrite,
+  handleClaimNumber,
+  handleSpecialDiscount,
+}) {
+  const [priceDifference, setPriceDifference] = useState(0);
 
-    const [priceDifference, setPriceDifference] = useState(0);
+  useEffect(() => {
+    let priceDiff =
+      Math.round(invoice.invoice_total) - Math.round(invoice.price_list_total);
+    setPriceDifference(priceDiff);
+  }, [invoice.invoice_total, invoice.price_list_total]);
 
-    useEffect(
-        ()=>{
-            let priceDiff = Math.round(invoice.invoice_total) - Math.round(invoice.price_list_total);
-            setPriceDifference(priceDiff);
-        },
-    [invoice.invoice_total, invoice.price_list_total]);
+  let price_match_component;
+  if (priceDifference > 0) {
+    price_match_component = (
+      <div>
+        Invoice Total is <strong>greater</strong> by &#8377;{" "}
+        {Math.abs(priceDifference)} &#10060;
+      </div>
+    );
+  } else if (priceDifference < 0) {
+    price_match_component = (
+      <div>
+        Invoice Total is <strong>lesser</strong> by &#8377;{" "}
+        {Math.abs(priceDifference)} &#10060;
+      </div>
+    );
+  } else if (priceDifference === 0) {
+    price_match_component = <div>Price match &#9989; </div>;
+  }
 
-    let price_match_component;
-    if (priceDifference > 0){
-        price_match_component =                 
-            <div>
-                Invoice Total is <strong>greater</strong> by  &#8377; {Math.abs(priceDifference)}   &#10060;
-            </div>
+  return (
+    <div className="purchase-invoice">
+      <header>
+        <strong className="invoice-number">
+          Invoice no. {invoice.invoice_number}
+        </strong>
+        {
+          <strong className="invoice-date">
+            Invoice Date:{" "}
+            <input
+              type="date"
+              value={invoice.invoice_date}
+              required="required"
+              onChange={(e) => handleInvoiceDate(invoice_index, e)}
+            />
+          </strong>
+        }
+        <br />
+        <br />
+      </header>
+      <hr />
 
-    }
-    else if (priceDifference < 0){
-        price_match_component =                 
-            <div>
-                Invoice Total is <strong>lesser</strong> by  &#8377; {Math.abs(priceDifference)}   &#10060;
-            </div>  
-    }
-    else if (priceDifference === 0){
-        price_match_component = <div>Price match  &#9989; </div>
-    }
-
-    return(
-        <div className="purchase-invoice">
-            <header>
-                <strong className="invoice-number">Invoice no. {invoice.invoice_number}</strong> 
-                {<strong className="invoice-date">Invoice Date: <input type="date" value={invoice.invoice_date} required="required" onChange={(e)=>handleInvoiceDate(invoice_index, e)}/></strong>}
-                <br/>
-                <br/>
-            </header>
-            <hr/>
-            
-            {(priceDifference&&invoice.special_discount)?
-            <div className="special-discount">Discount Type:
-                <input type="text" value={invoice.special_discount_type} onChange={(e)=>handleSpecialDiscount(invoice_index, e)} placeholder="ex - LVD" /> 
-            </div>
-            :null
-            }
-            {(priceDifference > 0)?     
-            <section className="claim-overwrite" onChange={(e)=>handleOverwrite(invoice_index,e)}>
-                <input type="checkbox" value="overwrite" name={"claim_overwrite"+invoice_index}/>
-                <label htmlFor={"claim_overwrite"+invoice_index}> Overwrite price list</label> 
-            </section>   
-            :(priceDifference < 0)?
-            <section className="claim-overwrite" onChange={(e)=>handleClaimOverwrite(invoice_index,e)}>
-                <input type="radio" id="claim" name={"claim_overwrite"+invoice_index} value={invoice.claim_invoice} required/> 
-                <label htmlFor={"claim"}> Claim Invoice</label>
-                <input type="radio" id="overwrite" name={"claim_overwrite"+invoice_index} value={invoice.overwrite_price_list} required/> 
-                <label htmlFor={"overwrite"}>Overwrite Price List</label>
-                <input type="radio" id="special_discount" name={"claim_overwrite"+invoice_index} value={invoice.special_discount} required/>
-                <label htmlFor={"special_discount"}>Special Discount</label>
-            </section>
-            :null              
-            }
-
-            <table className="invoice-item-headers">
-                <tr>
-                    <th>S.no.</th>
-                    <th>Item Desc</th>
-                    <th>Qty</th>
-                    <th>Price</th>
-                    {invoice.claim_invoice?
-                    <th>Claim No.</th>:null}
-                </tr>
-                
-                {invoice.claim_invoice?
-                invoice.claim_items.map( (service, claim_item_index)=>
-                <tr key={claim_item_index}>
-                    <td>{claim_item_index+1}</td>
-                    <td>{service.item_desc}:</td> 
-                    <td>{service.quantity}</td>
-                    <td>{service.item_total}</td>
-                    <td><input type="text" placeholder="claim number" onChange={e=>handleClaimNumber(invoice_index, claim_item_index, e)}/></td>
-                </tr>)
-                :invoice.items.map( (service, item_index)=>
-                <tr key={item_index}>
-                    <td>{item_index+1}</td>
-                    <td>{service.item_desc}:</td> 
-                    <td>{service.quantity}</td>
-                    <td>{service.item_total}</td>
-                </tr>)}
-
-                <tr>
-                    <th colSpan="3">Total</th>
-                    <th>{invoice.invoice_total}</th>
-                    {invoice.claim_invoice?
-                    <th></th>:null}
-                </tr>
- 
-            </table>
-
-            <section className="invoice-price-difference">
-                {price_match_component}
-            </section>
-
-            <br/>                           
+      {priceDifference && invoice.special_discount ? (
+        <div className="special-discount">
+          Discount Type:
+          <input
+            type="text"
+            value={invoice.special_discount_type}
+            onChange={(e) => handleSpecialDiscount(invoice_index, e)}
+            placeholder="ex - LVD"
+          />
         </div>
-    )
+      ) : null}
+      {priceDifference > 0 ? (
+        <section
+          className="claim-overwrite"
+          onChange={(e) => handleOverwrite(invoice_index, e)}
+        >
+          <input
+            type="checkbox"
+            value="overwrite"
+            name={"claim_overwrite" + invoice_index}
+          />
+          <label htmlFor={"claim_overwrite" + invoice_index}>
+            {" "}
+            Overwrite price list
+          </label>
+        </section>
+      ) : priceDifference < 0 ? (
+        <section
+          className="claim-overwrite"
+          onChange={(e) => handleClaimOverwrite(invoice_index, e)}
+        >
+          <input
+            type="radio"
+            id="claim"
+            name={"claim_overwrite" + invoice_index}
+            value={invoice.claim_invoice}
+            required
+          />
+          <label htmlFor={"claim"}> Claim Invoice</label>
+          <input
+            type="radio"
+            id="overwrite"
+            name={"claim_overwrite" + invoice_index}
+            value={invoice.overwrite_price_list}
+            required
+          />
+          <label htmlFor={"overwrite"}>Overwrite Price List</label>
+          <input
+            type="radio"
+            id="special_discount"
+            name={"claim_overwrite" + invoice_index}
+            value={invoice.special_discount}
+            required
+          />
+          <label htmlFor={"special_discount"}>Special Discount</label>
+        </section>
+      ) : null}
+
+      <table className="invoice-item-headers">
+        <thead>
+          <tr>
+            <th>S.no.</th>
+            <th>Item Desc</th>
+            <th>Qty</th>
+            <th>Price</th>
+            {invoice.claim_invoice ? <th>Claim No.</th> : null}
+          </tr>
+        </thead>
+
+        <tbody>
+          {invoice.claim_invoice
+            ? invoice.claim_items.map((service, claim_item_index) => (
+                <tr key={claim_item_index}>
+                  <td>{claim_item_index + 1}</td>
+                  <td>{service.item_desc}:</td>
+                  <td>{service.quantity}</td>
+                  <td>{service.item_total}</td>
+                  <td>
+                    <input
+                      type="text"
+                      placeholder="claim number"
+                      onChange={(e) =>
+                        handleClaimNumber(invoice_index, claim_item_index, e)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))
+            : invoice.items.map((service, item_index) => (
+                <tr key={item_index}>
+                  <td>{item_index + 1}</td>
+                  <td>{service.item_desc}:</td>
+                  <td>{service.quantity}</td>
+                  <td>{service.item_total}</td>
+                </tr>
+              ))}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <th colSpan="3">Total</th>
+            <th>{invoice.invoice_total}</th>
+            {invoice.claim_invoice ? <th></th> : null}
+          </tr>
+        </tfoot>
+      </table>
+
+      <section className="invoice-price-difference">
+        {price_match_component}
+      </section>
+
+      <br />
+    </div>
+  );
 }
 
 export default PurchaseInvoice;
