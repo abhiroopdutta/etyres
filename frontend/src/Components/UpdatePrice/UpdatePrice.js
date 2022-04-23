ac27532ebbd1a4296cc4aaa3160aa63c46d998c4
abhiroopdutta <abhiroopd@ymail.com>
2022-04-23 17:25:59 +0530

    fixed minor typo

--- frontend/src/Components/UpdatePrice/UpdatePrice.js
+++ frontend/src/Components/UpdatePrice/UpdatePrice.js
@@ -1,56 +1,67 @@
-import React, {useState} from 'react';
-import './UpdatePrice.css'
+import React, { useState } from "react";
+import "./UpdatePrice.css";
 
 function UpdatePrice() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [successMsg, setSuccessMsg] = useState("");
+  const [showLoader, setShowLoader] = useState(false);
 
-    const [selectedFile, setSelectedFile] = useState();
-    const [successMsg, setSuccessMsg] = useState("");
-    const [toggleLoader, setToggleLoader] = useState(false);
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-	const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-	};
+  const handleSubmission = (e) => {
+    e.preventDefault();
+    setShowLoader(true);
+    const formData = new FormData();
+    formData.append("file", selectedFile);
 
-    const handleSubmission = (e) => {
-        e.preventDefault();
-        setToggleLoader(true);
-		const formData = new FormData();
-		formData.append('file', selectedFile);
+    fetch("/api/update_price", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setShowLoader(false);
+        alert(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-		fetch(
-			'/api/update_price',
-			{
-				method: 'POST',
-				body: formData,
-			}
-            )
-            .then((response) => response.json())
-			.then((result) => {
-                setToggleLoader(false);
-                alert(result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-            
-
-
-	};
-
-    return (
-        <div className="update-price">
-            <h3>Upload price list xlsx to update price or add new items in inventory</h3>
-            <form method="POST" action="" encType="multipart/form-data" >
-            <p><input type="file" name="file" onChange={changeHandler}/></p>
-            <p><input type="submit" value="Submit" onClick={handleSubmission}/></p>
-            <p>{successMsg}</p>
-            {toggleLoader?
-            <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-            :null}
-            </form>
-        </div>
-        
-    );
+  return (
+    <div className="update-price">
+      <h3>
+        Upload price list xlsx to update price or add new items in inventory
+      </h3>
+      <form method="POST" action="" encType="multipart/form-data">
+        <p>
+          <input type="file" name="file" onChange={changeHandler} />
+        </p>
+        <p>
+          <input type="submit" value="Submit" onClick={handleSubmission} />
+        </p>
+        <p>{successMsg}</p>
+        {showLoader ? (
+          <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        ) : null}
+      </form>
+    </div>
+  );
 }
 
 export default UpdatePrice;
