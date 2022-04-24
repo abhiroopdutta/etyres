18570991f94c2818240c3221efd8b18d39277107
abhiroopdutta <abhiroopd@ymail.com>
2022-04-24 17:53:37 +0530

    added GST info for services

--- frontend/src/Components/CreateOrder/CartContext.js
+++ frontend/src/Components/CreateOrder/CartContext.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 export const CartContext = React.createContext();
 
 export const CartProvider = (props) => {
-
-    const [cart, setCart] = useState([]);
-    const [services, setServices] = useState([
-        {name:"Fitting", price:0, quantity:0, HSN:"998714"}, 
-        {name:"Balancing", price:0, quantity:0, HSN:"998714"},
-        {name:"Weights", price:0, quantity:0, HSN:"-"},
-        {name:"Alignment", price:0, quantity:0, HSN:"998714"}])
-    return(
-        <CartContext.Provider value={ {tyresContext:[cart, setCart], servicesContext:[services, setServices]} }>
-            {props.children}
-        </CartContext.Provider>
-        
-    )
-}
+  const [cart, setCart] = useState([]);
+  const [services, setServices] = useState([
+    { name: "Fitting", price: 0, quantity: 0, HSN: "998714", GST: "0.18" },
+    { name: "Balancing", price: 0, quantity: 0, HSN: "998714", GST: "0.18" },
+    { name: "Weights", price: 0, quantity: 0, HSN: "-", GST: "0.0" },
+    { name: "Alignment", price: 0, quantity: 0, HSN: "998714", GST: "0.18" },
+  ]);
+  return (
+    <CartContext.Provider
+      value={{
+        tyresContext: [cart, setCart],
+        servicesContext: [services, setServices],
+      }}
+    >
+      {props.children}
+    </CartContext.Provider>
+  );
+};
