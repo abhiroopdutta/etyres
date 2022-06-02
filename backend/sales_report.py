e14ffacf3561c6586b86ad2a5fd031ec6b7c18ef
abhiroopdutta <abhiroopd@ymail.com>
2022-06-02 17:22:22 +0530

    sales report bug fixes

--- backend/sales_report.py
+++ backend/sales_report.py
@@ -1,11 +1,11 @@
-from unittest import result
-from openpyxl.worksheet import worksheet
+from create_order import compute_gst_tables
 from models import Purchase, Sale, Product
 from mongoengine import Q
 import datetime
 import openpyxl
 import os
 from openpyxl.styles import Border, Side, PatternFill, Font
+from openpyxl.styles.numbers import FORMAT_PERCENTAGE, FORMAT_NUMBER
 wb = openpyxl.Workbook() 
 
 def get_sales_report(filters = {}, sorters = {}, pageRequest = 1, maxItemsPerPage = 5):
@@ -88,19 +88,48 @@ def report_handler(report_req_info):
             return export_purchase_report(results["data"])
         return results
 
-def export_sales_report(invoices):  
+def export_sales_report(invoices):
     file_base_dir = "./tempdata/sales_report/"
     filename = str(datetime.datetime.now())+"sales_report.xlsx"
     wb = openpyxl.Workbook() 
     sheet = wb.active
+    sheet.title = "Sales Report"
     sheet.freeze_panes = 'A2'
 
-    column_headers = ["Invoice No.", "Invoice Date", "Item Description", "HSN", "Rate per Item", "Qty", "Taxable Val", "CGST Rate", "CGST Amt", "SGST Rate", "SGST Amt", "IGST Rate", "IGST Amt", "Total", "Customer Name", "GSTIN"]
-    
-    #Aplly style to header row
-    sheet.row_dimensions[1].fill = PatternFill("solid", fgColor="b4f05c")
-    sheet.row_dimensions[1].border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
-    sheet.row_dimensions[1].font = Font(name="Calibri", bold=True)
+    column_headers = [
+        "Invoice No.", 
+        "Invoice Date", 
+        "Customer Name", 
+        "GSTIN", 
+        "Item Description", 
+        "Item Code", 
+        "HSN", 
+        "Qty", 
+        "Rate per Item", 
+        "Taxable Val", 
+        "CGST Rate", 
+        "CGST Amt", 
+        "SGST Rate", 
+        "SGST Amt", 
+        "IGST Rate", 
+        "IGST Amt", 
+        "Total"]
+
+    item_keys = [
+        "itemDesc",
+        "itemCode",
+        "HSN",
+        "quantity",
+        "ratePerItem",
+        "taxableValue",
+        "CGST",
+        "CGSTAmount",
+        "SGST",
+        "SGSTAmount",
+        "IGST",
+        "IGSTAmount",
+        "value",
+    ]
 
     for i, column_header in enumerate(column_headers):
         sheet.cell(row=1, column=i+1).value = column_header
@@ -108,86 +137,66 @@ def export_sales_report(invoices):
     #find a better way to do the following, instead of using base index, use sheet.max_row ?
     base_index = 0
     for invoice in invoices:
-        total_items = len(invoice.productItems) + len(invoice.serviceItems)
-        for i, product in enumerate(invoice.productItems):
+        gst_tables = compute_gst_tables(invoice.productItems, invoice.serviceItems)
+        if (invoice.customerDetails.GSTIN == "" or invoice.customerDetails.GSTIN.startswith("09")):
+            tax_table = gst_tables["GST_table"]
+        else:
+            tax_table = gst_tables["IGST_table"]
+        products = tax_table["products"]
+        services = tax_table["services"]
+
+        total_items = len(products) + len(services)
+        for i, product in enumerate(products):
             row_index = i+2+base_index
             sheet.cell(row = row_index, column = 1).value = invoice.invoiceNumber
             sheet.cell(row = row_index, column = 2).value = invoice.invoiceDate.strftime("%d/%m/%Y")
-            sheet.cell(row = row_index, column = 3).value = product.itemDesc
-            sheet.cell(row = row_index, column = 4).value = product.HSN
-            sheet.cell(row = row_index, column = 5).value = product.ratePerItem
-            sheet.cell(row = row_index, column = 6).value = product.quantity
-            taxable_val = round((float(product.quantity)*float(product.ratePerItem)),2)
-            CGST = round((float(product.CGST)*taxable_val),2)
-            SGST = round((float(product.SGST)*taxable_val),2)
-            IGST = round((float(product.IGST)*taxable_val),2)
-            total = taxable_val + SGST + CGST + IGST
-            sheet.cell(row = row_index, column = 7).value = taxable_val
-            sheet.cell(row = row_index, column = 8).value = str(round(product.CGST*100,2))+"%"
-            sheet.cell(row = row_index, column = 9).value = CGST
-            sheet.cell(row = row_index, column = 10).value = str(round(product.SGST*100,2))+"%"
-            sheet.cell(row = row_index, column = 11).value = SGST
-            sheet.cell(row = row_index, column = 12).value = str(round(product.IGST*100,2))+"%"
-            sheet.cell(row = row_index, column = 13).value = IGST
-            sheet.cell(row = row_index, column = 14).value = total
-            sheet.cell(row = row_index, column = 15).value = invoice.customerDetails.name
-            sheet.cell(row = row_index, column = 16).value = invoice.customerDetails.GSTIN
+            sheet.cell(row = row_index, column = 3).value = invoice.customerDetails.name
+            sheet.cell(row = row_index, column = 4).value = invoice.customerDetails.GSTIN
+
+            for j, item_key in enumerate(item_keys):
+                sheet.cell(row = row_index, column = 5+j).value = product[item_key]
             
-        for i, service in enumerate(invoice.serviceItems):
-            row_index = i+2+base_index+len(invoice.productItems)
+        for i, service in enumerate(services):
+            row_index = i+2+base_index+len(products)
             sheet.cell(row = row_index, column = 1).value = invoice.invoiceNumber
             sheet.cell(row = row_index, column = 2).value = invoice.invoiceDate.strftime("%d/%m/%Y")
-            sheet.cell(row = row_index, column = 3).value = service.name
-            sheet.cell(row = row_index, column = 4).value = service.HSN
-            sheet.cell(row = row_index, column = 5).value = service.ratePerItem
-            sheet.cell(row = row_index, column = 6).value = service.quantity
-            taxable_val = round((float(service.quantity)*float(service.ratePerItem)),2)
-            CGST = round((float(service.CGST)*taxable_val),2)
-            SGST = round((float(service.SGST)*taxable_val),2)
-            IGST = 0
-            total = taxable_val + SGST + CGST + IGST
-            sheet.cell(row = row_index, column = 7).value = taxable_val
-            sheet.cell(row = row_index, column = 8).value = str(round(service.CGST*100,2))+"%"
-            sheet.cell(row = row_index, column = 9).value = CGST
-            sheet.cell(row = row_index, column = 10).value = str(round(service.SGST*100,2))+"%"
-            sheet.cell(row = row_index, column = 11).value = SGST
-            sheet.cell(row = row_index, column = 12).value = "0.0%"
-            sheet.cell(row = row_index, column = 13).value = IGST
-            sheet.cell(row = row_index, column = 14).value = total
-            sheet.cell(row = row_index, column = 15).value = invoice.customerDetails.name
-            sheet.cell(row = row_index, column = 16).value = invoice.customerDetails.GSTIN
+            sheet.cell(row = row_index, column = 3).value = invoice.customerDetails.name
+            sheet.cell(row = row_index, column = 4).value = invoice.customerDetails.GSTIN
 
+            for j, item_key in enumerate(item_keys):
+                sheet.cell(row = row_index, column = 5+j).value = service[item_key]
 
         base_index += total_items
 
     total_row_index = sheet.max_row
     
-    #apply styles to final row
-    sheet.row_dimensions[total_row_index+1].fill = PatternFill("solid", fgColor="b4f05c")
-    sheet.row_dimensions[total_row_index+1].border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
-    sheet.row_dimensions[total_row_index+1].font = Font(name="Calibri", bold=True)
     # total row label
-    sheet.cell(row=total_row_index+1, column=2).value = "TOTAL"
+    sheet.cell(row=total_row_index+1, column=7).value = "TOTAL"
     # total quantity
-    total_quantity = '= SUM(F2:F'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=6).value = total_quantity
+    total_quantity = '= SUM(H2:H'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=8).value = total_quantity
     # total taxable val
-    total_taxable_val = '= SUM(G2:G'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=7).value = total_taxable_val
+    total_taxable_val = '= SUM(J2:J'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=10).value = total_taxable_val
     # total CGST
-    total_CGST = '= SUM(I2:I'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=9).value = total_CGST
+    total_CGST = '= SUM(L2:L'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=12).value = total_CGST
     # total SGST
-    total_SGST = '= SUM(K2:K'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=11).value = total_SGST
+    total_SGST = '= SUM(N2:N'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=14).value = total_SGST
     # total IGST
-    total_IGST = '= SUM(M2:M'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=13).value = total_IGST
+    total_IGST = '= SUM(P2:P'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=16).value = total_IGST
     # total 
-    total = '= SUM(N2:N'+str(total_row_index)+')'
-    sheet.cell(row=total_row_index+1, column=14).value = total
+    total = '= SUM(Q2:Q'+str(total_row_index)+')'
+    sheet.cell(row=total_row_index+1, column=17).value = total
 
-        #this adjusts the font and column width of the whole sheet
+    for i in range(2, sheet.max_row+1):
+        sheet.cell(row=i, column=11).number_format = FORMAT_PERCENTAGE
+        sheet.cell(row=i, column=13).number_format = FORMAT_PERCENTAGE
+        sheet.cell(row=i, column=15).number_format = FORMAT_PERCENTAGE
+
+    #this adjusts the font and column width of the whole sheet
     dims = {}
     for row in sheet.rows:
         for cell in row:
@@ -196,6 +205,17 @@ def export_sales_report(invoices):
     for col, value in dims.items():
         sheet.column_dimensions[col].width = value
     ####
+
+    # Apply styles to header and footer row
+    for col in range(1, sheet.max_column + 1):
+        cell_header = sheet.cell(1, col)
+        cell_header.fill = PatternFill("solid", fgColor="b4f05c")
+        cell_header.border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
+        cell_header.font = Font(name="Calibri", bold=True)
+        cell_footer = sheet.cell(sheet.max_row, col)
+        cell_footer.fill = PatternFill("solid", fgColor="b4f05c")
+        cell_footer.border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
+        cell_footer.font = Font(name="Calibri", bold=True)
 
     wb.save(file_base_dir+filename)
     return filename
@@ -210,11 +230,6 @@ def export_purchase_report(invoices):
 
     column_headers = ["Invoice No.", "Invoice Date", "Claim Invoice", "Item Description", "Item Code", "HSN", "Qty", "Taxable Val", "Tax", "Total"]
     
-    #Aplly style to header row
-    sheet.row_dimensions[1].fill = PatternFill("solid", fgColor="C5C5C5")
-    sheet.row_dimensions[1].border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
-    sheet.row_dimensions[1].font = Font(name="Calibri", bold=True)
-
     for i, column_header in enumerate(column_headers):
         sheet.cell(row=1, column=i+1).value = column_header
 
@@ -229,7 +244,7 @@ def export_purchase_report(invoices):
             sheet.cell(row = row_index, column = 3).value = "No" if invoice.claimInvoice ==0 else "Yes"
             sheet.cell(row = row_index, column = 4).value = product.itemDesc
             sheet.cell(row = row_index, column = 5).value = product.itemCode
-            sheet.cell(row = row_index, column = 6).value = product.HSN
+            sheet.cell(row = row_index, column = 6).value = int(product.HSN)
             sheet.cell(row = row_index, column = 7).value = product.quantity
             sheet.cell(row = row_index, column = 8).value = product.taxableValue
             sheet.cell(row = row_index, column = 9).value = product.tax
@@ -238,10 +253,6 @@ def export_purchase_report(invoices):
 
     total_row_index = sheet.max_row
     
-    #apply styles to final row
-    sheet.row_dimensions[total_row_index+1].fill = PatternFill("solid", fgColor="C5C5C5")
-    sheet.row_dimensions[total_row_index+1].border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
-    sheet.row_dimensions[total_row_index+1].font = Font(name="Calibri", bold=True)
     # total row label
     sheet.cell(row=total_row_index+1, column=2).value = "TOTAL"
     # total quantity
@@ -257,6 +268,9 @@ def export_purchase_report(invoices):
     total = '= SUM(J2:J'+str(total_row_index)+')'
     sheet.cell(row=total_row_index+1, column=10).value = total
 
+    for i in range(2, sheet.max_row+1):
+        sheet.cell(row=i, column=6).number_format = FORMAT_NUMBER
+
     #this adjusts the font and column width of the whole sheet
     dims = {}
     for row in sheet.rows:
@@ -266,6 +280,17 @@ def export_purchase_report(invoices):
     for col, value in dims.items():
         sheet.column_dimensions[col].width = value
     ####
+
+    # Apply styles to header and footer row
+    for col in range(1, sheet.max_column + 1):
+        cell_header = sheet.cell(1, col)
+        cell_header.fill = PatternFill("solid", fgColor="C5C5C5")
+        cell_header.border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
+        cell_header.font = Font(name="Calibri", bold=True)
+        cell_footer = sheet.cell(sheet.max_row, col)
+        cell_footer.fill = PatternFill("solid", fgColor="C5C5C5")
+        cell_footer.border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
+        cell_footer.font = Font(name="Calibri", bold=True)
 
     wb.save(file_base_dir+filename)
     return filename
@@ -290,11 +315,6 @@ def stock_report():
 
     sheet.freeze_panes = 'A2'
     column_headers = ["Item Description", "Item Code", "Cost Price", "Stock"]
-    
-    #Aplly style to header row
-    sheet.row_dimensions[1].fill = PatternFill("solid", fgColor="fcfa8e")
-    sheet.row_dimensions[1].border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
-    sheet.row_dimensions[1].font = Font(bold=True)
 
     for i, column_header in enumerate(column_headers):
         sheet.cell(row=1, column=i+1).value = column_header
@@ -315,6 +335,13 @@ def stock_report():
     for col, value in dims.items():
         sheet.column_dimensions[col].width = value
     ####
+
+    # Apply styles to header and footer row
+    for col in range(1, sheet.max_column + 1):
+        cell_header = sheet.cell(1, col)
+        cell_header.fill = PatternFill("solid", fgColor="fcfa8e")
+        cell_header.border = Border(left=Side(border_style="thin"),right=Side(border_style="thin"),top=Side(border_style="thin"),bottom=Side(border_style="thin"))
+        cell_header.font = Font(name="Calibri", bold=True)
 
     wb.save(file_base_dir+filename)
     return filename
