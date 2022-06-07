77b91ada6e6f90599c818608127a49fd62d6deea
abhiroopdutta <abhiroopd@ymail.com>
2022-06-07 23:25:22 +0530

    improved navbar css

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -5,12 +5,20 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navBar">
-      <h1>ETyres</h1>
-      <div className="Links">
-        <Link to="/reports">Reports</Link>
-        <Link to="/update_price">Update Price List</Link>
-        <Link to="/update_stock">Update Purchases</Link>
-        <Link to="/create_order">Create Order</Link>
+      <h1 className="nav-header">ETyres</h1>
+      <div className="nav-links-container">
+        <Link className="nav-link" to="/create_order">
+          Create Order
+        </Link>
+        <Link className="nav-link" to="/update_stock">
+          Update Purchases
+        </Link>
+        <Link className="nav-link" to="/update_price">
+          Update Price List
+        </Link>
+        <Link className="nav-link" to="/reports">
+          Reports
+        </Link>
       </div>
     </nav>
   );
