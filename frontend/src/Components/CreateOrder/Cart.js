d677935eb79412fd8c35d2aa447619913f9c887d
abhiroopdutta <abhiroopd@ymail.com>
2022-06-09 14:31:55 +0530

    fixed minor css issues

--- frontend/src/Components/CreateOrder/Cart.js
+++ frontend/src/Components/CreateOrder/Cart.js
@@ -18,7 +18,7 @@ function Cart({ handleRefreshProducts }) {
 
   const [previewInvoice, setPreviewInvoice] = useState(false);
   const transitions = useTransition(previewInvoice, {
-    config: { mass: 1, tension: 500, friction: 60, clamp: true },
+    config: { mass: 1, tension: 500, friction: 40, clamp: true },
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
@@ -218,7 +218,6 @@ function Cart({ handleRefreshProducts }) {
         </div>
 
         <div className="cart-total">TOTAL: &#x20B9;{cartTotal}</div>
-        <br />
       </div>
     </div>
   );
